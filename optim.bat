a4b0570b94feffe0ccd937443d1f4bac25d438fa
drabble <antoine.drabble@gmail.com>
2024-01-21 12:49:12 +0100

    Improve videos

--- optim.bat
+++ optim.bat
@@ -3,8 +3,10 @@ for /r src\assets\posts %%i in (*.jpg *.jpeg *.png *.avif) do (
    del "%%i"
 )
 for /r src\assets\posts %%i in (*.mp4 *.avi *.mkv) do (
-   ffmpeg -i "%%i" -c:v libx264 -crf 23 -c:a aac -b:a 128k "%%~ni.mp4"
+   ffmpeg -i "%%i" -c:v libx264 -crf 23 -c:a aac -b:a 128k "%%~dpi%%~ni_temp.mp4" -y
+   move /y "%%~dpi%%~ni_temp.mp4" "%%i"
 )
 for /r src\assets\posts %%i in (thumbnail.mp4) do (
-   ffmpeg -i "%%i" -c:v copy -an "%%~ni.mp4" -y
+   ffmpeg -i "%%i" -c:v copy -an "%%~dpi%%~ni_temp.mp4" -y
+   move /y "%%~dpi%%~ni_temp.mp4" "%%i"
 )
